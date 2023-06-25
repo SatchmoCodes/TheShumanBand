3c42248f18ca62f5ce0cce2a0acb4f1be1fd269a
SatchmoCodes <130078518+SatchmoCodes@users.noreply.github.com>
2023-06-25 12:42:06 -0400

    Add files via upload
    initial upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll('.loadIn');
+const options = {};
+
+const observer = new IntersectionObserver(function
+(entries, observer){
+entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        if (entry.target.classList.contains('image')) {
+            entry.target.classList.add('slideRight')
+        }
+        else if (entry.target.parentElement.classList.contains('quote')) {
+            entry.target.tagName == 'H3' ? entry.target.classList.add('dropDown') : entry.target.classList.add('slideLeft')
+        }
+        else {
+            entry.target.classList.add('appear')
+        }
+    }
+});
+}, options);
+
+// loop 
+sections.forEach( section => {
+observer.observe(section);
+});
