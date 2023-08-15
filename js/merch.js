56a9b0d8f4d65a22e4f0e272c362736646bb8821
SatchmoCodes <130078518+SatchmoCodes@users.noreply.github.com>
2023-08-14 23:07:27 -0400

    Update merch.js
    link questions

--- js/merch.js
+++ js/merch.js
@@ -3,3 +3,12 @@ import changeMenu from './main.js'
 document.querySelector('.hamButton').addEventListener('click', () => {
     changeMenu()
 })
+
+document.querySelectorAll('.shirtLink').forEach(e => {
+    e.addEventListener('click', () => {
+        let url = e.dataset.link
+        if (window.confirm("This link will take you to rootwithus.com, an affiliated Merch Store. Would you like to continue?")) {
+            window.open(url)
+        }
+    })
+})
